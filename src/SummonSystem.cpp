8887d201ada8401b1aa38872687891cf515aa49b
Pablo Sanchez <pablo.r.sanchez@hotmail.com>
2026-06-19 11:49:18 +0200

    practice summon

--- src/SummonSystem.cpp
+++ src/SummonSystem.cpp
@@ -28,16 +28,68 @@ Reglas de gameplay
                                             Regla 6
     El SummonTracker NO debe impedir que una invocación sea destruida.*/
 #include <memory>
+#include <array>
 #include <iostream>
-
+enum ECreatureType
+{
+    Wolf,Skeleton,Golem,
+};
+class Summon;
+// lifetime controller
 class Player
 {
+    std::unique_ptr<Summon> Summons;
+    public:
+    void SummonCreature(ECreatureType new_type)
+    {
+        Summons = std::make_unique<Summon>(new_type);
+        std::cout << "Player : I have summoned a : " << new_type << "\n";
+    }
+    ~Player()
+    {
+        std::cout << "Player : I died!\n"; 
+    }
 };
 
 class Summon
 {
+    ECreatureType Type;
+    public:
+    Summon(ECreatureType new_type):Type(new_type)
+    {
+        std::cout << "Summon : I am a :" << Type << "\n";
+    }
+    ECreatureType GetType() const {return Type;}
+    ~Summon()
+    {
+        std::cout << "Summon : I died too\n";
+    }
 };
 
+// observer
 class SummonTracker
 {
+    Summon* SummonOnStage;
+    public:
+    void RegisterSummon(Summon* new_summon)
+    {
+        if(new_summon)
+        {
+            SummonOnStage = new_summon;
+            std::cout << "SummonTracker : " << new_summon->GetType() << "Summoned\n";
+        }
+    }
+    void UnregisterSummon(Summon* summon)
+    {
+        if(SummonOnStage == summon)
+        {
+            std::cout << "SummonTracker : " << SummonOnStage->GetType() << "Unregistered\n";
+            SummonOnStage = nullptr;
+        }
+    }
 };
+
+int main()
+{
+    
+}
